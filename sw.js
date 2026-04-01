5c1341144bf0c18c6949364585a39f3a1c981b8b
LeMaxNutrition <maxime.vaslin.pro@gmail.com>
2026-04-01 17:55:09 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'nutri-v1';
+const CACHE = 'nutri-v3';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.svg', './icon-512.svg'];
 
 self.addEventListener('install', e => {
